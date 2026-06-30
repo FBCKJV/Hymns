fb7291005cc743eb188957822377d0da60dea82c
FBCKJV <coondawg68@gmail.com>
2026-06-29 21:25:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // FBC Hymns & Spiritual Songs — Service Worker v16
-const CACHE_NAME = 'ifb-hymns-v17';
+const CACHE_NAME = 'ifb-hymns-v18';
 const SHELL = ['/Hymns/','/Hymns/index.html','/Hymns/manifest.json','/Hymns/icons/icon-192.png','/Hymns/icons/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
