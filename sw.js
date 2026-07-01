ca33a451b0c802e82e1164982eabb36486bcd33a
FBCKJV <coondawg68@gmail.com>
2026-07-01 09:51:42 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ifb-hymns-v31';
+const CACHE_NAME = 'ifb-hymns-v33';
 const SHELL = ['/Hymns/','/Hymns/index.html','/Hymns/manifest.json','/Hymns/icons/icon-192.png','/Hymns/icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
@@ -15,7 +15,7 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).then(r => {
       if (r.ok) {
-        const clone = r.clone(); // clone BEFORE consuming
+        const clone = r.clone();
         caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
       }
       return r;
